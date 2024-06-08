239080ae5091e7322fb364e609dada17be42f04d
Anas-Karoum <anas-karoum@outlook.com>
2024-06-08 11:35:00 +0200

    upphiften är klar

---
+++ VerificationProvider/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using VerificationProvider.Contexts;
+using VerificationProvider.Services;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("VerificationDatabase")));
+        services.AddScoped<IVerificationService, VerificationService>();
+        services.AddScoped<ICleanerService, CleanerService>();
+        services.AddScoped<IValidateCodeService, ValidateCodeService>();
+
+
+    })
+    .Build();
+
+
+
+using (var scope = host.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        var migration = context.Database.GetPendingMigrations();
+        if (migration != null && migration.Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        Debug.WriteLine($"ERROR :: VerificationProvider.Program.cs :: {ex.Message}");
+    }
+}
+
+host.Run();
